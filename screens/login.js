6631551b54b462a4cf8ee19cbdf17014a455a548
unknown <josuecallado@hotmail.com>
2024-05-25 11:35:33 -0600

    Mamu :v

--- screens/login.js
+++ screens/login.js
@@ -1,47 +1,62 @@
-//autenticacion
-import * as WebBrowser from 'expo-web-browser';
-import * as Google from 'expo-auth-session/providers/google'
-
-import { StatusBar } from "expo-status-bar";
 import React, { useState, useEffect } from "react";
 import { View, Text, TouchableOpacity, TextInput, StyleSheet, ImageBackground } from "react-native";
-import { GoogleSignin, GoogleSigninButton } from '@react-native-google-signin/google-signin';
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { login, register } from "../api/user_api"; // Asegúrate de tener estos módulos implementados y exportados
+import { GoogleSignin, GoogleSigninButton, statusCodes  } from '@react-native-google-signin/google-signin';
 import { styles } from "../styles/loginstyles"; // Importando los estilos
 import SvgComponent from './SVGComponet';
 
-WebBrowser.maybeCompleteAuthSession();
+export default function Login({ navigation }) {
+    const [error, setError] = useState(null);
+    const [userInfo, setUserInfo] = useState(null);
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    
+    useEffect(() => {
+        GoogleSignin.configure({
+            webClientId: '467750875455-d7ou19di6ckc6v1eo22ufvpjl8bo6blb.apps.googleusercontent.com',
+        });
 
-const Login = ({ setIsAuthenticated }) => {
-  const [accessToken, setAccessToken] = useState(null);
-  const [user, setUser] = useState(null);
+        const checkIfLoggedIn = async ()=>{
+          try {
+            const isSignedIn = await GoogleSignin.isSignedIn();
+            if (isSignedIn) {
+                const userInfo = await GoogleSignin.signInSilently();
+                setUserInfo(userInfo);
+                navigation.navigate('MainTaps'); 
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    };
+        checkIfLoggedIn();
 
-  const [ response] = Google.useIdTokenAuthRequest({
-    clientId: "467750875455-d7ou19di6ckc6v1eo22ufvpjl8bo6blb.apps.googleusercontent.com",
-    androidClientId: "467750875455-5e0n1euhv4ntlvhvnjfnge6d79susjlf.apps.googleusercontent.com",
-  });
 
-  useEffect(() => {
-    if (response?.type === "success") {
-      setAccessToken(response.authentication.accessToken);
-      accessToken && fetchUserInfo();
-    }
-  }, [response, accessToken]);
+    }, [navigation]);
 
-  async function fetchUserInfo() {
-    let response = await fetch("https://www.googleapis.com/userinfo/v2/me", {
-      headers: {
-        Authorization: `Bearer ${accessToken}`
-      }
-    });
-    const userInfo = await response.json();
-    setUser(userInfo);
-
-    // Redirigir a la pestaña de "Explore" después de la autenticación exitosa
-    setIsAuthenticated(true);
-  }
    
+
+    const signin = async () => {
+        try {
+            await GoogleSignin.hasPlayServices();
+            const user = await GoogleSignin.signIn();
+            setUserInfo(user);
+            setError(null);
+            setIsLoggedIn(true);
+            navigation.navigate('MainTaps');
+        } catch (e) {
+            setError(e);
+        }
+    }
+
+    const logout = async () => {
+      try {
+          await GoogleSignin.revokeAccess();
+          await GoogleSignin.signOut();
+          setUserInfo(null);
+      } catch (error) {
+          setError(error);
+      }
+  }
+
     return (
       <View style={styles.container}> 
       <SvgComponent/>
@@ -49,19 +64,18 @@ const Login = ({ setIsAuthenticated }) => {
       <Text style={styles.subTiltle}>Sign in to your account</Text>
       <View style={styles.containerButton}>
       <GoogleSigninButton
-          size={GoogleSigninButton.Size.Standard}
-          color={GoogleSigninButton.Color.Light}
-          onPress={signin}
-          style={styles.googleButton}
-          />
-          <TouchableOpacity style={styles.logoutButton} onPress={logout}>
-              <Text style={styles.logoutButtonText}>Logout</Text>
-          </TouchableOpacity>
-          {error && <Text style={styles.errorText}>{JSON.stringify(error)}</Text>}
-          {userInfo && <Text style={styles.userInfo}>{JSON.stringify(userInfo.user)}</Text>}
-          </View>
-         </View>
+            size={GoogleSigninButton.Size.Standard}
+            color={GoogleSigninButton.Color.Light}
+            onPress={signin}
+            style={styles.googleButton}
+            />
+            <TouchableOpacity style={styles.logoutButton} onPress={logout}>
+                <Text style={styles.logoutButtonText}>Logout</Text>
+            </TouchableOpacity>
+            {error && <Text style={styles.errorText}>{JSON.stringify(error)}</Text>}
+            {userInfo && <Text style={styles.userInfo}>{JSON.stringify(userInfo.user)}</Text>}
+           </View>
+      </View>
     );
 }
 
-export default Login;
