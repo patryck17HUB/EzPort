e6bc2ee35a75ec302fbf054793fc1fbf79d9a933
Diego Julian Pescador Cordova <143102789+DiegoPez@users.noreply.github.com>
2024-05-25 21:04:47 -0600

    botones

--- styles/explorestyles.js
+++ styles/explorestyles.js
@@ -1,6 +1,7 @@
 import { StyleSheet } from "react-native";
 import { Color, FontSize, FontFamily } from "./GlobalStyles";
 
+
 export const styles = StyleSheet.create({
   home1: {
     fontSize: 32,
@@ -13,9 +14,12 @@ export const styles = StyleSheet.create({
     marginBottom: 20,
   },
   container: {
-    flex: 1,
+    flex: 5,
     justifyContent: "center",
     alignItems: "center",
+    marginTop: '0%',
+    marginBottom: '24%',
+    
   },
   signout: {
     backgroundColor: Color.secondary,
@@ -23,6 +27,68 @@ export const styles = StyleSheet.create({
     borderRadius: 10,
     margin: 10,
   },
+    button: {
+      backgroundColor: '#7539e5',
+      padding: 15,
+      borderRadius: 5,
+      width: '80%',
+    },
+    buttonText: {
+      color: '#fff',
+      fontSize: 16,
+    },
+    modalContainer: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: 'rgba(0, 0, 0, 0.5)',
+      
+    },
+    modalContent: {
+      width: '80%',
+      backgroundColor: 'white',
+      borderRadius: 10,
+      padding: 20,
+      alignItems: 'center',
+    },
+    option: {
+      padding: 15,
+      borderBottomWidth: 1,
+      borderBottomColor: '#ddd',
+    },
+    optionText: {
+      fontSize: 16,
+    },
+
+    // Botones de cada ejercicio
+    exerciseItem: {
+      margin: 10,
+      width: '100%',
+      justifyContent: "center",
+    alignItems: "center",
+    },
+    exerciseText: {
+      fontSize: 16,
+      color: '#fff',
+    },
+    exercisePText: {
+      fontSize: 16,
+      color: '#000',
+    },
+    exerciseList: {
+      marginTop: '10%',
+      justifyContent: "center",
+      alignSelf: 'center', 
+      width: '100%',
+    },
+    gradient: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    
+   
+    
 });
 
 /*   
