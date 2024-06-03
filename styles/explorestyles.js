16a33daa4b9421c9d3a1cbf6d8ee96a0aa8c7fa2
unknown <dpescador28@alumnos.uaq.mx>
2024-06-03 15:24:40 -0600

    axploreAMLO

--- styles/explorestyles.js
+++ styles/explorestyles.js
@@ -1,8 +1,51 @@
 import { StyleSheet } from "react-native";
 import { Color, FontSize, FontFamily } from "./GlobalStyles";
 
-
 export const styles = StyleSheet.create({
+  option: {
+    padding: 20,
+    borderBottomWidth: 1,
+    borderBottomColor: '#7539e5',
+    marginHorizontal: 15,
+    borderRadius: 10,
+    backgroundColor: Color.primary,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+},
+optionText: {
+    fontSize: FontSize.size_base,
+    color: '#fff',
+    fontFamily: FontFamily.font_base,
+},
+icon: {
+    position: 'absolute',
+    right: 10,
+},
+content: {
+  paddingHorizontal: 20,
+  paddingVertical: 10,
+  backgroundColor: '${Color.secondary}CC',
+  marginHorizontal: 20,
+  borderBottomLeftRadius: 15,
+  borderBottomRightRadius: 15,
+  
+},
+exerciseItem: {
+  margin: 10,
+  width: '100%',
+  justifyContent: "center",
+  alignItems: "center",
+},
+
+contentText: {
+  marginLeft: 0, // Mantén el margen izquierdo en 20
+  marginRight: 0,
+  color: '#fff',
+  fontSize: FontSize.size_base,
+  fontFamily: FontFamily.font_base,
+},
+
   home1: {
     fontSize: 32,
     color: "#FFFFFF",
@@ -19,6 +62,7 @@ export const styles = StyleSheet.create({
     alignItems: "center",
     marginTop: '0%',
     marginBottom: '24%',
+    
   },
   signout: {
     backgroundColor: Color.secondary,
@@ -26,79 +70,78 @@ export const styles = StyleSheet.create({
     borderRadius: 10,
     margin: 10,
   },
-    button: {
-      backgroundColor: '#7539e5',
-      padding: 15,
-      borderRadius: 5,
-      width: '80%',
-    },
-    buttonText: {
-      color: '#fff',
-      fontSize: 16,
-    },
-    modalContainer: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: 'rgba(0, 0, 0, 0.5)',
-      
-    },
-    modalContent: {
-      width: '80%',
-      backgroundColor: 'white',
-      borderRadius: 10,
-      padding: 20,
-      alignItems: 'center',
-    },
-    option: {
-      padding: 15,
-      borderBottomWidth: 1,
-      borderBottomColor: '#ddd',
-    },
-    optionText: {
-      fontSize: 16,
-    },
-    // Botones de cada ejercicio
-    exerciseItem: {
-      margin: 10,
-      width: '100%',
-      justifyContent: "center",
-    alignItems: "center",
-    },
-    exerciseText: {
-      fontSize: 16,
-      color: '#fff',
-    },
-    exercisePText: {
-      fontSize: 16,
-      color: '#000',
-    },
-    exerciseList: {
-      marginTop: '10%',
-      justifyContent: "center",
-      alignSelf: 'center', 
-      width: '100%',
-    },
-    gradient: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-    buttonchueco: {
-      backgroundColor: '#7539e5',
-      padding: 15,
-      marginRight: '5.5%',
-      borderRadius: 5,
-      width: '80%',
-    },
-    botongift: {
-      backgroundColor: '#7539e5',
-      padding: 15,
-      margin:5,
-      borderRadius: 5,
-      width: '80%',
-    },
+  button: {
+    marginLeft: '3%', // Mantén el margen izquierdo en 20
+    marginRight: '9%',
+    padding: 5,
+    borderRadius: 5,
+    borderColor: '#fff', 
+    width: '100%',
+    borderColor: '#fff', // Color del borde
     
+    borderBottomWidth: 2, 
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+    marginRight: '19%'
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  modalContent: {
+    width: '80%',
+    backgroundColor: 'white',
+    borderRadius: 10,
+    padding: 20,
+    alignItems: 'center',
+  },
+  
+  // Botones de cada ejercicio
+  exerciseItem: {
+    margin: 10,
+    width: '100%',
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  exerciseText: {
+    fontSize: 16,
+    color: '#fff',
+  },
+  exercisePText: {
+    fontSize: 16,
+    color: '#000',
+    marginRight: '9%'
+  },
+  exerciseList: {
+    marginTop: '10%',
+    justifyContent: "center",
+    alignSelf: 'center',
+    width: '100%',
+  },
+  gradient: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonchueco: {
+    backgroundColor: '#7539e5',
+    padding: 15,
+    marginRight: '5.5%',
+    borderRadius: 5,
+    width: '80%',
+  },
+  botongift: {
+    backgroundColor: '#7539e5',
+    padding: 15,
+    margin: 5,
+    borderRadius: 5,
+    width: '80%',
+  },
+  
 
 });
 
